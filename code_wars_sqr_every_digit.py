6e74164b5de57fa84403babb2447d840ac00c5f7
Christian G <chrisgobin94@gmail.com>
2021-07-14 11:08:16 -0400

    Update code_wars_sqr_every_digit.py
    finished problem

--- code_wars_sqr_every_digit.py
+++ code_wars_sqr_every_digit.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 # Example: input 9119 -> 811181
-def square_digits(num_str):
-	digit_arr = num_str.split()
-	
-	return "".join([i**2 for i in digit_arr])
-		
-	
-	
-	
-	
-	
-	
-print(square_digits("9112"))
+def sqr_nums(str_num_seq):
+    arr_of_nums = [str(int(i)**2) for i in str_num_seq]
+    answer = "".join(arr_of_nums)
+    return answer
+
+
+some_input = "2234"
+sqr_nums(some_input)
