de4c0ad552d0a71c744f00e917f7e818f00c03d7
Christian G <chrisgobin94@gmail.com>
2021-07-16 10:07:16 -0400

    Update code_wars_regex.py

--- code_wars_regex.py
+++ code_wars_regex.py
@@ -1,7 +1,7 @@
 """
 def validate_pin(pin):
     my_pin = [int(c) for c in pin]
-    if(len(my_pin) != 4 or len(my_pin) != 6):
+    if(len(my_pin) != 4 and len(my_pin) != 6):
         return false
         bool_pin_check = [item.isdigit() for item in my_pin]
         print(bool_pin_check)
@@ -16,6 +16,19 @@ print(validate_pin("1234"))
 """
 my_input = "1351"
 my_input_transformed = [int(c) for c in my_input]
-
+for i in my_input_transformed:
+    if(type(i) == int):
+        print(True)
+is_a_number = type(my_input_transformed)
+print(is_a_number)
 print(my_input_transformed)
 print(len(my_input_transformed))
+length_of_input = len(my_input_transformed)
+
+if length_of_input != 4 and length_of_input != 6:
+    print(False)
+else:
+
+    print(True)
+    
+ #Check if all items in list are digits. 
