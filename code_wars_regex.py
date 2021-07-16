12d33bc6aed828adb7157d792623c5701a9cd671
Christian G <chrisgobin94@gmail.com>
2021-07-16 00:57:58 -0400

    Update code_wars_regex.py
    updated function

--- code_wars_regex.py
+++ code_wars_regex.py
@@ -1,12 +1,21 @@
+"""
 def validate_pin(pin):
-    if(pin.length() != 4 or pin.length() != 6):
+    my_pin = [int(c) for c in pin]
+    if(len(my_pin) != 4 or len(my_pin) != 6):
         return false
-    bool_pin_check = [item.isdigit() for item in pin]
-    print(bool_pin_check)
+        bool_pin_check = [item.isdigit() for item in my_pin]
+        print(bool_pin_check)
     else:
-        for item in pin:
+        for item in my_pin:
             if(item.isdigit() == True):
                 return true
             else:
                 return false
    
+print(validate_pin("1234"))
+"""
+my_input = "1351"
+my_input_transformed = [int(c) for c in my_input]
+
+print(my_input_transformed)
+print(len(my_input_transformed))
