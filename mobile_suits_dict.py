4a83021933109be72be3d88ea569e067b72a15ee
Christian G <chrisgobin94@gmail.com>
2021-07-23 10:37:49 -0400

    Create mobile_suits_dict.py

---
+++ mobile_suits_dict.py
@@ -0,0 +1,13 @@
+# This is going to be an api consisting of Mobile Suits from the Gundam Series and their pilots.
+# mbs_data == mobile_suit_data
+
+mbs_data = {
+	'Amuro' : 'RX-78-2',
+	'Char' : 'Red-Comet',
+	'XeonSoldiers' : 'Zaku',
+	'Hero' : ['Wing-Gundam-1', 'Epyon'],
+	'Zechs', ['Tall-Geese', 'Epyon'],
+	'Trowa' : 'Heavy-Arms',
+	'Duo' : 'Death-Scythe,
+	'AI-ML' : ['Leo', 'Mobile Suit Dolls']
+}
