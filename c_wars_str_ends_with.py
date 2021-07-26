634eee677cf52163856057360c61d2d3a52ed66e
Christian G <chrisgobin94@gmail.com>
2021-07-26 11:25:31 -0400

    Update c_wars_str_ends_with.py

--- c_wars_str_ends_with.py
+++ c_wars_str_ends_with.py
@@ -1,7 +1,12 @@
 def solution(string, ending):
-    last_letter = string[-1]
-    last_ending= ending[-1]
-    if ending in string:
-        if last_letter == last_ending:
-            return True
+    end_len = len(ending) - 1
+    str_len = len(string) - 1
+    
+    # slice notation.
+    # get 
+    if ending in string and string[end_len : -1] == ending:
+        return True 
     return False
+
+# Thinking: Take slice of string from end and check if equal to ending string
+# Also handle the empty string error. Should be True not false// All the time
