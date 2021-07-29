49b5c7280bc8b6a4289e05a3b27c97feddf53ced
Christian G <chrisgobin94@gmail.com>
2021-07-29 12:16:59 -0400

    Create c_wars_ten_min_walk.py

---
+++ c_wars_ten_min_walk.py
@@ -0,0 +1,7 @@
+def is_valid_walk(walk):
+    valid_moves = ['n', 's', 'e', 'w']
+    for move in valid_moves
+    if ['n', 's', 'e', 'w'] in walk:
+        if len(walk) >= 10:
+            return True
+        elif len(walk) < 10:
