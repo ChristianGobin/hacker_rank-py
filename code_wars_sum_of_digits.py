0f721df5bd905bba3b2aaed9faeabb18edf34fa2
Christian G <chrisgobin94@gmail.com>
2021-08-05 12:16:37 -0400

    Update code_wars_sum_of_digits.py

--- code_wars_sum_of_digits.py
+++ code_wars_sum_of_digits.py
@@ -1,3 +1,18 @@
 def digital_root(n):
     # Keep adding the contents of n until the len = 1
     answer = 0
+
+    """
+    Take input -> turn int to string -> split to array -> have running count of total -> keep computing until len(total) = 1
+    x = 942
+    z = str(x)
+    y = z.split()
+    total  = 0
+    while 
+    for i in y:
+        for x in i:
+            total = total + int(x)
+    
+    print(total)
+    """
+    
