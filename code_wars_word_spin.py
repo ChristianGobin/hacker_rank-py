6e779155d232f56e34e47cff9124dc527325a34d
Christian G <chrisgobin94@gmail.com>
2022-01-31 13:10:12 -0500

    Update code_wars_word_spin.py

--- code_wars_word_spin.py
+++ code_wars_word_spin.py
@@ -1,6 +1,14 @@
-def spin_words(sentence):
-    sent_arr = sentence.split(" ")
-    for word in sent_arr:
-        if word.length >= 5:
-            
-    return None
+my_str = "Hello"
+def reverse(_str):
+  reversed = []
+  index = len(_str)
+  while index > 0:
+    reversed.append(_str[index - 1])
+    index = index - 1
+  
+  reversed_string = ""
+  reversed_string = reversed_string.join(reversed)
+  print reversed_string
+
+
+reverse("Hello world as")
