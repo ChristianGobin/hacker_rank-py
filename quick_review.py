a86be7697d50759e9b50a476d41806edad1e16d8
Christian G <chrisgobin94@gmail.com>
2022-11-09 22:09:17 -0500

    Create quick_review.py

---
+++ quick_review.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Primitive Data Types - String, Int, Bool, float
+
+x = 33
+name = "christian"
+my_float = float(32.21)
+
+def conversion(some_int, some_float):
+	return float(some_int + some_float)
+
+def printf(string_statement, ):
+	
+print(conversion(x, my_float))
+
+def square_num(num):
+	print(num ** 2)
+	return num ** 2
+
+
